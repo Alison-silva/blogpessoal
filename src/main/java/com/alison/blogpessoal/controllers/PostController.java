ad6ad9f43015568f0b17c5d532ec015079e4ea48
Alison-silva <alison.ssilva@hotmail.com>
2024-04-05 23:55:59 -0300

    atualizações de registro

--- src/main/java/com/alison/blogpessoal/controllers/PostController.java
+++ src/main/java/com/alison/blogpessoal/controllers/PostController.java
@@ -1,16 +1,43 @@
 package com.alison.blogpessoal.controllers;
 
+import com.alison.blogpessoal.model.Post;
+import com.alison.blogpessoal.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
 @Controller
 public class PostController {
+
+    @Autowired
+    private PostService postService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("posts", postService.list());
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "registerpost")
+    public ModelAndView registerpost() {
+        ModelAndView modelAndView = new ModelAndView("registerpost");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "register")
+    public ModelAndView register(Post post){
+        ModelAndView modelAndView = new ModelAndView("registerpost");
+
+        Date now = new Date();
+        post.setTimestamp(now);
+
+        postService.save(post);
         return modelAndView;
     }
 
--- src/main/java/com/alison/blogpessoal/services/PostService.java
+++ src/main/java/com/alison/blogpessoal/services/PostService.java
@@ -1,4 +1,24 @@
 package com.alison.blogpessoal.services;
 
+import com.alison.blogpessoal.model.Post;
+import com.alison.blogpessoal.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post save(Post post){
+        return postRepository.save(post);
+    }
+
+    public List<Post> list(){
+        return postRepository.findAll();
+    }
+
 }
