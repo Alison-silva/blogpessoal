50a88940e7865cf5eaf7bdff6e95dbe4a4740053
Alison-silva <alison.ssilva@hotmail.com>
2024-04-08 16:15:11 -0300

    Login em memória

---
+++ src/main/java/com/alison/blogpessoal/security/WebConfigSecurity.java
@@ -0,0 +1,44 @@
+package com.alison.blogpessoal.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/", "/postpag", "/posts/{id}").permitAll()
+                .antMatchers(HttpMethod.POST, "/findpost").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .loginPage("/login")
+                .failureUrl("/login?error=true")
+                .and().logout().logoutSuccessUrl("/login")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("teste")
+                .password("123")
+                .roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+}
